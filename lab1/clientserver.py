2094fa73deb5511db48771f54e70e9d93b5c18e8
Yusuf Basgöncü <yenesbas@gmail.com>
2025-11-01 17:06:45 +0100

    GET and GETALL functions

--- lab1/clientserver.py
+++ lab1/clientserver.py
@@ -10,7 +10,7 @@ from context import lab_logging
 
 lab_logging.setup(stream_level=logging.INFO)  # init loging channels for the lab
 
-# pylint: disable=logging-not-lazy, line-too-long
+#pylint: disable=logging-not-lazy, line-too-long
 
 class Server:
     """ The server """
@@ -24,6 +24,73 @@ class Server:
         self.sock.settimeout(3)  # time out in order not to block forever
         self._logger.info("Server bound to socket " + str(self.sock))
 
+        self.telefonbuch = {
+            "Hakim": 13245,
+            "Florian": 13245,
+            "Yusuf": 13245, 
+            "Korn": 13245,
+            "Weber": 54321,
+            "Hofmann": 15643,
+            "Mueller": 49122,
+            "Schmidt": 49233,
+            "Schneider": 49344,
+            "Fischer": 49455,
+            "Meyer": 49566,
+            "Wagner": 49677,
+            "Becker": 49788,
+            "Schulz": 49899,
+            "Hoffmann": 49910,
+            "Koch": 50011,
+            "Bauer": 50022,
+            "Richter": 50033,
+            "Klein": 50044,
+            "Wolf": 50055,
+            "Neumann": 50066,
+            "Schwarz": 50077,
+            "Zimmermann": 50088,
+            "Braun": 50099,
+            "Krueger": 50110,
+            "Hartmann": 50132,
+            "Lange": 50143,
+            "Schmitt": 50154,
+            "Werner": 50165,
+            "Schmitz": 50176,
+            "Krause": 50187,
+            "Meier": 50198,
+            "Lehmann": 50209,
+            "Schmid": 50220,
+            "Schaefer": 50231,
+            "Maier": 50242,
+            "Keller": 50253,
+            "Huber": 50264,
+            "Mayer": 50275,
+            "Kaiser": 50286,
+            "Vogel": 50297,
+            "Fuchs": 50308,
+            "Lang": 50319,
+            "Weiss": 50330,
+            "Peters": 50341,
+            "Scholz": 50352,
+            "Jung": 50363,
+            "Moeller": 50374,
+            "Hahn": 50385,
+            "Koenig": 50396,
+            "Walter": 50407,
+            "Kaiser": 50418
+        }
+
+
+
+    def handle_request(self, request):
+        if request.startswith("GETALL"):
+            entries = [f"{name}: {nummer}" for name, nummer in self.telefonbuch.items()]
+            return "\n".join(entries)
+        elif request.startswith("GET"):
+            name_list = request.split(" ")
+            name = name_list[1]
+            number = self.telefonbuch.get(name)
+            return f"{name}: {number}\n"
+
     def serve(self):
         """ Serve echo """
         self.sock.listen(1)
@@ -35,7 +102,8 @@ class Server:
                     data = connection.recv(1024)  # receive data from client
                     if not data:
                         break  # stop if client stopped
-                    connection.send(data + "*".encode('ascii'))  # return sent data plus an "*"
+                    response = self.handle_request(data.decode('ascii'))
+                    connection.send(response.encode('ascii'))  # return sent data plus an "*"
                 connection.close()  # close the connection
             except socket.timeout:
                 pass  # ignore timeouts
@@ -52,15 +120,22 @@ class Client:
         self.sock.connect((const_cs.HOST, const_cs.PORT))
         self.logger.info("Client connected to socket " + str(self.sock))
 
-    def call(self, msg_in="Hello, world"):
+    def send_request(self, request):
         """ Call server """
-        self.sock.send(msg_in.encode('ascii'))  # send encoded string as data
+        self.sock.send(request.encode('ascii'))  # send encoded string as data
         data = self.sock.recv(1024)  # receive the response
-        msg_out = data.decode('ascii')
-        print(msg_out)  # print the result
+        response = data.decode('ascii')
         self.sock.close()  # close the connection
         self.logger.info("Client down.")
-        return msg_out
+        return response
+
+    def GET(self, name):
+        response = self.send_request(f"GET {name}")
+        print(response)
+    
+    def GETALL(self):
+        response = self.send_request("GETALL")
+        print(response)
 
     def close(self):
         """ Close socket """
