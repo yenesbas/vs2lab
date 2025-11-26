d7a4c99906d6da5657a6684e4cede1bb1680d20c
Yusuf Basgöncü <yenesbas@gmail.com>
2025-11-26 11:55:48 +0100

    lab2

--- lab2/rpc/runcl.py
+++ lab2/rpc/runcl.py
@@ -1,7 +1,15 @@
 import rpc
 import logging
+import time
 
 from context import lab_logging
+
+def callback(result_tuple):
+    # result_tuple is (APPEND, DBList)
+    if len(result_tuple) > 1:
+        print("Result: {}".format(result_tuple[1].value))
+    else:
+        print("Result: {}".format(result_tuple))
 
 lab_logging.setup(stream_level=logging.INFO)
 
@@ -9,8 +17,10 @@ cl = rpc.Client()
 cl.run()
 
 base_list = rpc.DBList({'foo'})
-result_list = cl.append('bar', base_list)
+cl.append('bar', base_list, callback)
 
-print("Result: {}".format(result_list.value))
+for i in range(15):
+    print("Client is doing other work...")
+    time.sleep(1)
 
 cl.stop()
